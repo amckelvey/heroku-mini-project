652ad736640c8123367f4190a4c77a074d795b00
Majelvey <mckelvey.andy@gmail.com>
2022-06-09 11:14:02 -0400

    second commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 // Import custom middleware, "cLog"
 app.use(clog);
 
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
